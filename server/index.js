fd69a43bc16538be51ca7aed28171833d357feb0
Trung Nguyen <trung.nguyen@terralogic.com>
2021-01-16 10:06:16 +0700

    Update heroku deployment

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({
+    Hello: 'I deployed my app !',
+  });
+});
+
 app.get('/api/user/auth', auth, (req, res) => {
   res.status(200).json({
     _id: req._id,
